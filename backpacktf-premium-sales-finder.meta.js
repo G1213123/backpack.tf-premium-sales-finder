81145766dce8ac175098f81fbea0f6983d565e83
G1213123 <49609970+G1213123@users.noreply.github.com>
2024-10-07 20:41:40 +0800

    Update backpacktf-premium-sales-finder.meta.js

--- backpacktf-premium-sales-finder.meta.js
+++ backpacktf-premium-sales-finder.meta.js
@@ -4,8 +4,8 @@
 // @version     4.1.1
 // @description Adds coloring to history pages indicating recent sales and includes compare links for sales
 // @author      Julia
-// @updateURL   https://github.com/juliarose/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.meta.js
-// @downloadURL https://github.com/juliarose/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.user.js
+// @updateURL   https://github.com/G1213123/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.meta.js
+// @downloadURL https://github.com/G1213123/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.user.js
 // @run-at      document-end
 // @grant       GM_addStyle
 // @grant       unsafeWindow
